3e296d0d0b91c3e273f5299603fea1e7427a27af
issmuth <ismael.mdev@gmail.com>
2023-06-16 12:33:09 -0700

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always success (0)
+ */
+int main()
+{
+	for (int a = 65; a < 92; a++)
+	{
+		putchar("%c", a);
+	}
+	return (0);
+}
