315e28848d2d479857b387944daa0cc56bc50ed1
issmuth <ismael.mdev@gmail.com>
2023-06-17 11:23:34 -0700

    Done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print out single digit numbers
+ * of base ten followed by a new line
+ *
+ * Return: 0 when succesful 1 otherwise
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a + '0' <= '9')
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+	return 0;
+}
