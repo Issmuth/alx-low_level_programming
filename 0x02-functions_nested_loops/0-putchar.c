2cd752c58c229e3903604cfdb9029692ae40e52a
issmuth <ismael.mdev@gmail.com>
2023-06-17 12:48:23 -0700

    Done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (success)
  */
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +16,4 @@ int main(void){
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
-	
+}	
