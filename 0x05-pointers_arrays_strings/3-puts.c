2f82018832f1101faab31cd9f4788f16b3160ba9
issmuth <ismael.mdev@gmail.com>
2023-06-18 12:16:19 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to character varibles which stores the string
+ * Return: NULL
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] =! '\0')
+	{
+		putchar(str[a]);
+		a++;
+	}
+	putchar('\n');
+}
