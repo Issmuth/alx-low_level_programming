6080608cea84ff151b7872275f49bace2e2d7a22
issmuth <ismael.mdev@gmail.com>
2023-06-18 10:06:55 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values on two variables
+ * @a:value to be swapped
+ * @b:value to be swapped
+ * Return: NULL
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	*a = c;
+	*b = *a;
+	*b = c;
+}
