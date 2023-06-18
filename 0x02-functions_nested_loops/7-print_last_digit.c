52855c534aa8af0f76700084bedd0f8f489555fc
issmuth <ismael.mdev@gmail.com>
2023-06-18 03:51:52 -0700

    Done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - extracts the last digit of a number
+ * @a: number to extract las digit from
+ * Return: the last digit
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
