66eb4d930aba109abf1c9736cc8c045aca2be4d5
issmuth <ismael.mdev@gmail.com>
2023-06-18 04:16:28 -0700

    Done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,19 @@
  * Return: the last digit
  */
 
-int print_last_digit(int a)
+int print_last_digit(int n)
 {
-	return (a % 10);
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	a = n % 10;
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+	return (a);
 }
