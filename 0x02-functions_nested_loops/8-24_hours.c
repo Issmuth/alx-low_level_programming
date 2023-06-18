f0ceb965c5121258a5821413979bed4a4c7444dd
issmuth <ismael.mdev@gmail.com>
2023-06-18 05:13:43 -0700

    Done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every complete 24 hours
+ * in the hh:mm format each minute on a new line
+ *
+ * Return: NULL
+*/
+
+void jack_bauer(void)
+{
+	int a,b,c,d;
+
+	for(int a=0; a<3; a++)
+	{
+		for (int b=0; b<10; b++)
+		{
+			if(a==2 && b==4){
+				break;
+			} else
+			{
+				for(int c=0; c<6; c++)
+				{
+					for(int d=0; d<10; d++)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(':');
+						putchar(c + '0');
+						putchar(d + '0');
+						putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
