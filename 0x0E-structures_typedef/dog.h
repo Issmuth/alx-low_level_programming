9c92e4835797dfba8f01db0176aa80f800233e2b
issmuth <ismael.mdev@gmail.com>
2023-06-20 01:36:34 -0700

    Done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - data type for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: each dog struct will store the name age and owner
+ * of a single dog enity
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
