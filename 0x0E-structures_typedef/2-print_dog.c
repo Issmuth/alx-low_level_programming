9065256537e28963b7cf167542b30ada311d593f
issmuth <ismael.mdev@gmail.com>
2023-06-20 02:26:05 -0700

    Done

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,26 +10,26 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d.name == NULL)
+		if (d->name == NULL)
 		{
 			printf("Name: (nil)");
 		} else
 		{
-			printf("Name: %s\n", d.name);
+			printf("Name: %s\n", d->name);
 		}
-		if (d.age == NULL)
+		if (d->age == NULL)
 		{
 			printf("Age: (nil)");
 		} else
 		{
-			printf("Age: %s\n", d.age);
+			printf("Age: %s\n", d->age);
 		}
-		if (d.owner == NULL)
+		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)");
 		} else
 		{
-			printf("Owner: %s\n", d.owner);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
