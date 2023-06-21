db381249a6f6a008e25ac4ac22e523ac11d6add5
issmuth <ismael.mdev@gmail.com>
2023-06-21 12:12:39 -0700

    Done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef function_pointers_h
+#define function_pointers_h
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 /**
@@ -5,3 +7,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
  */
 
 typedef int size_t;
+#endif
