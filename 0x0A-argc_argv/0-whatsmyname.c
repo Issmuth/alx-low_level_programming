acb215d3f2267603ea8bfc9df9e72b77b20a541f
issmuth <ismael.mdev@gmail.com>
2023-06-22 05:56:09 -0700

    Done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  * Return: Always Success (0)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
