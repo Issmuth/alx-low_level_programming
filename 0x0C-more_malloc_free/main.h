bc59e6aaf0601831bcf9abafbbd50f99f38f5f1a
issmuth <ismael.mdev@gmail.com>
2023-06-24 23:35:16 -0700

    Done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,14 +3,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 void *malloc_checked(unsigned int b);
-#endif#ifndef MALLOC
-#define MALLOC
-#include <stdio.h>
-#include <stdlib.h>
-void *malloc_checked(unsigned int b);
-#endif#ifndef MALLOC
-#define MALLOC
-#include <stdio.h>
-#include <stdlib.h>
-void *malloc_checked(unsigned int b);
 #endif
