bc0f264425c261c39603f6c535b94f24c94369f9
issmuth <ismael.mdev@gmail.com>
2023-07-05 15:15:50 -0700

    optimus prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * prime_check - scans through possible dividers
+ * @n: number to be checked
+ * @i: initial vallue
+ *
+ * Return: 1 if prime 0 if not
+ */
+
+int prime_check(int n, int i)
+{
+	if ((n % i) == 0 && i != n)
+		return (0);
+
+	if ((n % i) != 0 && i < n)
+		return (prime_check(n, (i + 1));
+
+	return (1);
+}
+
+/**
+ * _is_prime_number - check if the number is prime
+ * @
+ *
+ *
+ */
+int is_prime_number(int n)
+{
+	return (prime_check(n, 2));
+}
