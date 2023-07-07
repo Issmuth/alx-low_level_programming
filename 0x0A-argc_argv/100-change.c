ea77b1e77827afb7d38f7b67e147a093b810bab8
issmuth <ismael.mdev@gmail.com>
2023-07-07 16:24:07 -0700

    now it finished

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,8 +19,7 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
-	} else
-	{
+	}
 		while (cents > 0)
 		{
 			change += 1;
@@ -29,25 +28,21 @@ int main(int argc, char *argv[])
 				cents -= 25;
 				continue;
 			}
-
 			if ((cents - 10) >= 0)
 			{
 				cents -= 10;
 				continue;
 			}
-			
 			if ((cents - 5) >= 0)
 			{
 				cents -= 5;
 				continue;
 			}
-
 			if ((cents - 2) >= 0)
 			{
 				cents -= 2;
 				continue;
 			}
-
 			if ((cents - 1) >= 0)
 			{
 				cents -= 1;
@@ -56,5 +51,4 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", change);
 		return (0);
-	}
 }
