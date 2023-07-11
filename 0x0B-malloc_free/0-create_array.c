5dd47631f4f39df809b5da0fe35ac886580eee69
issmuth <ismael.mdev@gmail.com>
2023-07-11 02:34:32 -0700

    malloced

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - returns pointer to the array
+ * @size: size of array
+ * @c: character
+ *
+ * Return: pointer to the array 
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr
+
+	if (size == 0)
+		return (NULL);
+	
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
+}
