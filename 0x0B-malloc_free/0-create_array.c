60899cfebba89fc29ab16d9bae22d4018daac111
issmuth <ismael.mdev@gmail.com>
2023-07-11 06:18:02 -0700

    mallocation fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,20 +5,25 @@
  * @size: size of array
  * @c: character
  *
- * Return: pointer to the array 
+ * Return: pointer to the array
  */
 
-char *create_array(unsigned int size, __attribute__((unused))char c)
+char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *ptr;
 
 	if (size == 0)
 		return (NULL);
-	
-	ptr = malloc(size * sizeof(char));
+
+	ptr = (char *) malloc(size * sizeof(char));
 
 	if (ptr == NULL)
 		return (NULL);
 
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
+	ptr[size - 1] = '\0';
 	return (ptr);
 }
