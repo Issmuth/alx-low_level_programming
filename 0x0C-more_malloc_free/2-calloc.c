7c0707ab4836d31d348b76211769563542ca6ebb
issmuth <ismael.mdev@gmail.com>
2023-07-13 12:56:00 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,18 @@
 #include "main.h"
+/**
+ * set_zero - sets the allcoted memory to zero
+ * @membs: each memory to be set zero
+ * @ptr: pointer to the array
+ *
+ */
+
+void set_zero(unsigned int membs, char *ptr)
+{
+	unsigned int i;
+
+	for (i = 0; i < membs; i++)
+		ptr[i] = 0;
+}
 
 /**
  * _calloc - allocates memory of an array
@@ -11,20 +25,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	ptr = malloc(nmemb * size);
 
-	if (ptr == NULL)
-		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
+	if (ptr == NULL)
 	{
-		 ptr[i] = 0;
+		return (NULL);
 	}
+
+	set_zero((nmemb * size), ptr);
 
 	return (ptr);
 }
