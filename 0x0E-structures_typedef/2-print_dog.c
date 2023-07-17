a6b5afbb68c0f261d17d51c887ca42961a0b8427
issmuth <ismael.mdev@gmail.com>
2023-07-17 08:53:21 -0700

    puppy printed

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints dog info
+ * @d: dog to be printed
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+	{
+		printf("nil\n");
+	} else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+	{
+		printf("nil\n");
+	} else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
