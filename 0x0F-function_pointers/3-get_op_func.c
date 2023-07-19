f4d135ebff18e85254025b19096c040a9271fb33
issmuth <ismael.mdev@gmail.com>
2023-07-19 12:32:13 -0700

    done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,29 +2,28 @@
 
 /**
  * get_op_func - selects correct function
- * 
+ *
  * Return: pointer to the function that corresponds
  * to the operator given as a parameter
  */
 
 int (*get_op_func(char *s))(int, int)
 {
-	 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while ( i < 6)
-    {
-	    if (s == ops[i].op)
-		    return(ops[i].f);
-	    i++;
-    }
-
-    return (NULL);
+	while (i < 6)
+	{
+		if (s == ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
