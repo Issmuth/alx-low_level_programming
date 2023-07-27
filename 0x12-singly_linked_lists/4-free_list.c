3206f0bc2e1baf8870ec2ef3e53e4625171ff525
issmuth <ismael.mdev@gmail.com>
2023-07-27 18:20:41 +0300

    done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list
+ * @head: list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *iterate;
+
+	while (head)
+	{
+		iterate = head->next;
+		free(head);
+		free(head->str);
+		head = iterate;
+	}
+}
