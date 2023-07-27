c020aa095566b93b6c8a7362194675055bc83da9
issmuth <ismael.mdev@gmail.com>
2023-07-27 23:37:18 +0300

    done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,25 @@
-
 section .data
     hello_message db "Hello, Holberton", 0
-    format_string db "%s", 0
+    new_line db 10, 0
+    format_string db "%s%s", 0
 
 section .text
-    extern printf, putchar
+    extern printf
+    global main
 
-global main
 main:
     push rbp
     mov rbp, rsp
 
-    lea rdi, [rel hello_message]
-    mov rax, 0
+    lea rdi, [rel format_string]
+    lea rsi, [rel hello_message]
+    lea rdx, [rel new_line]
+    xor eax, eax
     call printf
 
-    mov eax, 0x0A  ; ASCII value of new line character
-    mov edi, eax
-    call putchar
+    mov eax, 0  ; return 0 to indicate successful execution
 
-    mov eax, 0     ; Return 0 to indicate successful execution
     leave
     ret
+
+
