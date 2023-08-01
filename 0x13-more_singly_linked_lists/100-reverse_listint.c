5dc9dd76d15104ff4755e8cae64c44ef084ce15d
issmuth <ismael.mdev@gmail.com>
2023-08-01 21:52:43 +0300

    done

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,25 +11,19 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev_node, *next_node;
 
-	prev_node = malloc(sizeof(listint_t));
-	if (prev_node == NULL)
-		return (NULL);
+	prev_node = NULL;
+	next_node = NULL;
 
 	if (*head == NULL)
 		return (NULL);
 
-	next_node = (*head)->next;
-	(*head)->next = NULL;
 	while (*head)
 	{
-		*head = prev_node;
-		*head = next_node;
-		if (*head == NULL)
-			continue;
-
 		next_node = (*head)->next;
 		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
 	}
 	*head = prev_node;
 	return (*head);
-}	
+}
