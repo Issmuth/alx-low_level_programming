8b6ab18b8217d1ca81c42950f45d2b2da75cca4b
issmuth <ismael.mdev@gmail.com>
2023-08-07 12:25:10 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,16 +11,16 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, len;
+	int file, write_count;
 	
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, filepermissions);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file < 0)
 		return (-1);
 	if (text_content == NULL)
-		text_content = "";
+		text_content = "\0";
 
 	write_count = write(file, text_content, strlen(text_content));
 
