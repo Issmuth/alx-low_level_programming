72f83dba493bd303c008b3969dc07fc1fe225ebd
issmuth <ismael.mdev@gmail.com>
2023-08-08 08:34:39 -0700

    done

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - set the bit to 1 at index
+ * @n: points to the integer at hand
+ * @index: position where the bit is set
+ *
+ * Return: 1 if successful -1 otherwise
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
+}
