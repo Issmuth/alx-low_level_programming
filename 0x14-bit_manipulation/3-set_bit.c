c95e01ecc056e28c8bd93f0df267806ea94902cc
issmuth <ismael.mdev@gmail.com>
2023-08-08 08:45:51 -0700

    done

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
+
 	*n |= (1 << index);
 	return (1);
 }
