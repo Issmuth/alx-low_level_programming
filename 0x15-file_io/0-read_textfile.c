ec51a9df8e8307471354d4718f4a78390295d1b1
issmuth <ismael.mdev@gmail.com>
2023-09-01 13:14:04 +0300

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - writes the content of a file
+ * to the POSIX stdout.
+ * @filename: file to be referenced
+ * @letters: number of letters to print
+ *
+ * Return: number of letters actually printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t actual;
+	char *buf;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		printf("memory allocation failed\n");
+		return (0);
+	}
+	
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	actual = read(fd, buf, letters);
+	if (actual < 0)
+		return (0);
+
+	actual = write(STDOUT_FILENO, buf, letters);
+	if (actual < 0)
+		return (0);
+
+	return (actual);
+}
