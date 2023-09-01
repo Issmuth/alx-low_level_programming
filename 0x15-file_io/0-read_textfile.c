bdbc93d2b241ea3c98972785017db3fa77f598b5
issmuth <ismael.mdev@gmail.com>
2023-09-01 13:18:42 +0300

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		printf("memory allocation failed\n");
 		return (0);
 	}
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
