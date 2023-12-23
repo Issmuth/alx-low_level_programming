1dbbc46256d49fea95e1598a169c5600a2ca44b7
issmuth <ismael.mdev@gmail.com>
2023-12-23 14:52:01 +0300

    fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,15 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	if (!ht)
+		return (NULL);
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t));
+	if (!(ht->array))
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
