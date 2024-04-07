a964e757d1164752b539b540161d3e8153023672
Issmuth <Ismael.mdev@gmail.com>
2024-04-07 09:01:25 +0300

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -8,6 +8,7 @@
  * Return: product
  */
 
-int mul(int a, int b){
+int mul(int a, int b)
+{
 	return (a * b);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number
+ */
+
+void print_numbers(void)
+{
+	char num = 0;
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		_putchar(num++);
+
+	_putchar('\n');
+}
