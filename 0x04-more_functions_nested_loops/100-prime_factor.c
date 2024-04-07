32e55d8fed598e731ce745a26c6067e68884d655
Issmuth <Ismael.mdev@gmail.com>
2024-04-07 11:04:28 +0300

    done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - ...
+ * Return: ...
+ */
+
+int main(void)
+{
+	long int num = 612852475143, i;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+			if (num == i)
+				break;
+
+			i = 2;
+		}
+	}
+	printf("%ld\n",num);
+	return (0);
+}
