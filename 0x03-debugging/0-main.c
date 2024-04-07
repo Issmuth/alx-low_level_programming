4d328bdf6c9284fa46c08c219f7d7abac34531c1
Issmuth <Ismael.mdev@gmail.com>
2024-04-07 06:36:23 +0300

    done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - zero test case for positive negagtive function
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
