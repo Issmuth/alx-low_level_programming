27d4481ba58e3f8bae2944e18493f72d16720a2f
Issmuth <Ismael.mdev@gmail.com>
2024-04-07 09:09:01 +0300

    doen

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ */
+
+void print_most_numbers(void)
+{
+	char num = '0';
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		if (i != 2 & i != 4)
+			_putchar(num++);
+
+	_putchar('\n');
+}
